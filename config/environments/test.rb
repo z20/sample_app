fa8faefa854aaa2c973854fb3191a4329258f6ab
Zach Holdren <zholdren20@gmail.com>
2015-04-19 01:29:56 +0000

    Add account activations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.action_mailer.default_url_options = { host: 'https://rails-tutorial-z20a.c9.io' }
+
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
